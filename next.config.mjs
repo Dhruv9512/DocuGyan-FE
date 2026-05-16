64139832d2eccfb6d1faf4971824888d983bb288
Dhruv Sharma <dhruvsharma56780@gmail.com>
2026-05-16 10:10:12 +0530

    Adding

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  trailingSlash: true,
   allowedDevOrigins: ["172.20.10.8"],
   async rewrites() {
     return [
